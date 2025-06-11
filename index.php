ccf8ebb821bbcc8ce1be0dcdc6efd4fd15ed92c3
NicolasLima01 <nicolasrlima@protonmail.com>
2025-06-11 11:20:45 -0300

    feat: adicionando fundo no rodape, grid e icones do insta e github

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Program East</title>
     <link rel="stylesheet" href="css/bootstrap.min.css"> <!-- arquivo css do bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"> <!-- icones do bootstrap -->
 </head>
 
 <body>
--- rodape.php
+++ rodape.php
@@ -1,3 +1,27 @@
-<footer class="container bg-black">
+<style>
+    .bi {
+        font-size: 30px;
+    }
 
-</footer>
+    .bi-instagram {
+        color: transparent;
+        background-clip: text;
+        background-image: linear-gradient(to right, #833ab4, #fd1d1d, #fcb045);
+    }
+
+    .bi:hover{
+        zoom: 150%;
+        /* transform: scale(2.2); */
+        transition: 0.5s;
+    }
+</style>
+
+<div class="container bg-black" style="height: 30vh;">
+    <div class="row">
+        <div class="col-6"></div>
+        <div class="col-6 d-flex justify-content-evenly">
+            <a href="https://www.instagram.com/program_east/" target="_blank"><i class="bi bi-instagram"></i></a>
+            <a href="https://github.com/ProgramEast" target="_blank"><i class="bi bi-github text-white"></i></a>
+        </div>
+    </div>
+</div>
