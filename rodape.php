0ffe41092600100af06caa21bf43e39760817506
NicolasLima01 <nicolasrlima@protonmail.com>
2025-06-12 11:27:28 -0300

    adicionando logo provisória no menu e rodapé

--- rodape.php
+++ rodape.php
@@ -9,19 +9,27 @@
         background-image: linear-gradient(to right, #833ab4, #fd1d1d, #fcb045);
     }
 
-    .bi:hover{
+    .bi:hover {
         zoom: 150%;
         /* transform: scale(2.2); */
         transition: 0.5s;
     }
 </style>
 
-<div class="container bg-black" style="height: 30vh;">
+<div class="container-fluid bg-black" style="height: 20vh;">
     <div class="row">
-        <div class="col-6"></div>
-        <div class="col-6 d-flex justify-content-evenly">
-            <a href="https://www.instagram.com/program_east/" target="_blank"><i class="bi bi-instagram"></i></a>
-            <a href="https://github.com/ProgramEast" target="_blank"><i class="bi bi-github text-white"></i></a>
+        <div class="col-6">
+            <!-- Logo -->
+            <a href="#"><img class="rounded float-end mt-3" src="img/logo_program_east.png" height="100px" width="100px" alt="Program East"></a>
+        </div>
+        <div class="col-6">
+            <p class="text-light fs-5 mt-2">Contato:</p>
+
+            <!-- icones para o contato -->
+            <div>
+                <a href="https://instagram.com/program_east/" target="_blank"><i class="bi bi-instagram me-2"></i></a>
+                <a href="https://github.com/ProgramEast" target="_blank"><i class="bi bi-github text-white"></i></a>
+            </div>
         </div>
     </div>
 </div>
