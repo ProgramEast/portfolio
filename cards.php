372ce7a3a08488739f406e46c536becea7f04305
NicolasLima01 <nicolasrlima@protonmail.com>
2025-06-13 13:33:28 -0300

    feat: Adicionando alert no arquivo de cards

--- cards.php
+++ cards.php
@@ -1,6 +1,10 @@
+<div class="container alert alert-primary mt-5 d-flex justify-content-center" role="alert">
+    <p class="fs-2 fw-bold mx-auto">Projetos</p>
+</div>
+
 <div class="container row row-cols-1 row-cols-md-2 g-4 mx-auto my-5" id="projetos">
     <div class="col">
-        <div class="card text-bg-dark border-primary">
+        <div class="card text-bg-primary border-primary">
             <img src="img/web_stayfit.png" class="card-img-top " alt="..." height="">
             <div class="card-body">
                 <h5 class="card-title">Site de Academia</h5>
@@ -10,7 +14,7 @@
     </div>
 
     <div class="col">
-        <div class="card text-bg-dark border-primary">
+        <div class="card text-bg-primary border-primary">
             <img src="img/desk_stayfit.png" class="card-img-top " alt="..." height="">
             <div class="card-body">
                 <h5 class="card-title">Sistema de Gestão de Academia</h5>
@@ -20,7 +24,7 @@
     </div>
 
     <div class="col">
-        <div class="card text-bg-dark border-primary">
+        <div class="card text-bg-primary border-primary">
             <img src="img/web_chuleta.png" class="card-img-top " alt="..." height="">
             <div class="card-body">
                 <h5 class="card-title">Site de Churrascaria</h5>
